5165a8da562295a7b4a2c98b178a6da5226d7b0c
Steven Mathew <mathewsteven92@gmail.com>
2025-06-03 17:36:10 -0400

    Updated Netlify CORS Middleware

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ import (
 )
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://trademinutes-auth.onrender.com")
+		w.Header().Set("Access-Control-Allow-Origin", "https://eliocloud.netlify.app")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
-		if r.Method == "OPTIONS" {
+		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
 		}
@@ -26,6 +26,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
 
 func main() {
 	// Load .env file
