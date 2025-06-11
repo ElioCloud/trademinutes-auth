fb931e494e89d886dd310c1fadf63e21c7518961
Steven Mathew <mathewsteven92@gmail.com>
2025-06-11 16:11:02 -0400

    Added Github Route

--- routes/auth_routes.go
+++ routes/auth_routes.go
@@ -10,11 +10,11 @@ import (
 
 func AuthRoutes(router *mux.Router) {
 	authRouter := router.PathPrefix("/api/auth").Subrouter()
+
 	authRouter.HandleFunc("/register", controllers.RegisterHandler).Methods("POST")
 	authRouter.HandleFunc("/login", controllers.LoginHandler).Methods("POST")
 	authRouter.Handle("/profile", middleware.JWTAuthMiddleware(http.HandlerFunc(controllers.ProfileHandler))).Methods("GET")
 	authRouter.HandleFunc("/forgot-password", controllers.ForgotPasswordHandler).Methods("POST")
 	authRouter.HandleFunc("/reset-password", controllers.ResetPasswordHandler).Methods("POST")
-
-
+	authRouter.HandleFunc("/github", controllers.GitHubOAuthHandler).Methods("POST")
 }
