6205ceaed0e5c858f9586cb528d8110ffe3248db
Steven Mathew <mathewsteven92@gmail.com>
2025-06-14 22:28:06 -0400

    Update main.go

--- main.go
+++ main.go
@@ -14,13 +14,7 @@ import (
 )
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-
-		// Allow only known origins
-		if origin == "https://eliocloud.netlify.app" || origin == "http://localhost:3000" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
@@ -32,7 +26,6 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
 func main() {
 	// Load .env file
 	if os.Getenv("ENV") != "production" {
